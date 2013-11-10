5afbb3bb5d4f1c7dafd470dfee6ed0346d26d86b
vencentle <vencentle@gmail.com>
2013-11-10 16:31:13 +0800

    add proj source

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by vencentle on 11/10/13.
+//  Copyright (c) 2013 vencentle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
