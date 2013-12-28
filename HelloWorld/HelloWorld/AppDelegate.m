4b5c74f13966ee76ee8f751d321d2c5e2debbddc
liwenxin <liwenxin@MBPR.local>
2013-12-28 19:25:02 +0800

    test..

--- HelloWorld/HelloWorld/AppDelegate.m
+++ HelloWorld/HelloWorld/AppDelegate.m
@@ -14,6 +14,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    // user commit test
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
